a4d13252b71357cd7674f82eb388ab3f43245fa8
Sergio Del Valle <sergio@nittenapps.dev>
2024-11-19 08:18:39 -0600

    feat: slide toggle

---
+++ projects/nittenapps/material/src/lib/slide-toogle/index.ts
@@ -0,0 +1,2 @@
+export { StackMatToggleModule } from './module';
+export { StackFieldToggle, StackToggleFieldConfig } from './slide-toggle.type';
--- projects/nittenapps/material/src/public-api.ts
+++ projects/nittenapps/material/src/public-api.ts
@@ -2,16 +2,13 @@
  * Public API Surface of material
  */
 
-export { StackAutocompleteFieldConfig, StackFieldAutocomplete, StackMatAutocompleteModule } from './lib/autocomplete';
-export { StackFieldButton, StackMatButtonModule } from './lib/button';
-export { StackDatepickerFieldConfig, StackFieldDatepicker, StackMatDatepickerModule } from './lib/datepicker';
-export {
-  StackDatetimepickerFieldConfig,
-  StackFieldDatetimepicker,
-  StackMatDatetimepickerModule,
-} from './lib/datetimepicker';
-export { FieldType, StackMatFormFieldModule } from './lib/form-field';
-export { StackMatInputModule } from './lib/input';
-export { StackFieldSelect, StackMatSelectModule, StackSelectFieldConfig } from './lib/select';
-export { StackMatTableModule } from './lib/table';
-export { StackMatTabsModule } from './lib/tabs';
+export * from './lib/autocomplete';
+export * from './lib/button';
+export * from './lib/datepicker';
+export * from './lib/datetimepicker';
+export * from './lib/form-field';
+export * from './lib/input';
+export * from './lib/select';
+export * from './lib/slide-toogle';
+export * from './lib/table';
+export * from './lib/tabs';
